6bc61151b2d730581c7414bc28dc521d57a2865e
axemanks <keithscheldt@gmail.com>
2023-04-06 10:16:34 -0400

    Initial commit

---
+++ src/Canvas.js
@@ -0,0 +1,24 @@
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Center } from "@react-three/drei";
+
+export const App = ({ position = [-1, 0, 2.5], fov = 25 }) => (
+  <Canvas
+    eventSource={document.getElementById("root")}
+    eventPrefix="client"
+    camera={{ position, fov }}
+  >
+    <Center>
+      <Shirt />
+    </Center>
+    <OrbitControls />
+  </Canvas>
+);
+
+function Shirt() {
+  return (
+    <mesh>
+      <boxGeometry args={[0.9, 0.9, 0.9]} />
+      <meshNormalMaterial />
+    </mesh>
+  );
+}
