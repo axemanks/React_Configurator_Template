20a530db4195759c5b2197f043b669d9bb3acecc
axemanks <keithscheldt@gmail.com>
2023-04-07 20:21:19 -0400

    texture changes working

--- src/Canvas.js
+++ src/Canvas.js
@@ -1,10 +1,13 @@
 import { Canvas, useFrame } from "@react-three/fiber";
-import { OrbitControls, Center, useGLTF, Environment, AccumulativeShadows, RandomizedLight } from "@react-three/drei";
+import { OrbitControls, Center, useGLTF, Environment, AccumulativeShadows, RandomizedLight, Decal, useTexture } from "@react-three/drei";
 import React, { useRef } from "react";
 import { easing } from "maath";
+import { useSnapshot } from "valtio";
+import state from "./store";
+import * as THREE from "three";
 
 // position = [-1, .5, 2.5],  - loads camera at a big angle to the model
-export const App = ({ position = [0, .5, 2.5], fov = 75 }) => (
+export const App = ({ position = [0, 0, 2.5], fov = 25 }) => (
   <Canvas
     shadows
     eventSource={document.getElementById("root")}
@@ -14,24 +17,38 @@ export const App = ({ position = [0, .5, 2.5], fov = 75 }) => (
     <ambientLight intensity={0.5} />
     <Environment preset="city" />
     <CameraRig>
-      <Center>      
-      <Shirt />
       <Backdrop />
+      <Center>      
+      <Shirt />      
       </Center>
       </CameraRig>
-    <OrbitControls />
+     {/* <OrbitControls /> */}
   </Canvas>
 );
 
 // backdrop
 function Backdrop() {
-  return( <AccumulativeShadows
+  const shadows = useRef()
+
+  useFrame((state, delta) =>
+    easing.dampC(
+      shadows.current.getMesh().material.color,
+      state.selectedColor,
+      0.25,
+      delta
+    )
+  )
+  
+
+
+  return (<AccumulativeShadows
+    ref={shadows}
     temporal // When temporal is set to true, the shadows will be smoothed out over time, resulting in less flickering and jittering
     frames={60} // fps
     alphaTest={0.85}
     scale={10} // scale of shadows
     rotation={[Math.PI / 2, 0, 0]}
-    position={[-.01, 0, -1]} // position of backdrop itself, last number moves it backward
+    position={[0, 0, -0.14]} // position of backdrop itself, last number moves it backward
   > 
     <RandomizedLight
       amount={4}
@@ -50,29 +67,14 @@ function Backdrop() {
   </AccumulativeShadows>
   )
 }
-
-function Shirt(props) {
-  const { nodes, materials } = useGLTF("/baseball_hat.glb");
-  return (
-    <group {...props} dispose={null}>
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.defaultMaterial.geometry}
-        material={materials.labert1}
-      />
-    </group>
-  );
-}
-
 // update the rotation property of a 3D object (referenced by the group.current object) based on the user's mouse movement
 function CameraRig({ children }) {
-  const group = useRef();
-  
+  const group = useRef();  
   useFrame((state, delta) => {
+    easing.damp3(state.camera.position, [0, 0, 2], 0.25, delta)
     easing.dampE(
       group.current.rotation,
-      [state.pointer.y / 3, -state.pointer.x / 3, 0], // higher numbers = less movement on mouse move
+      [state.pointer.y / 10, -state.pointer.x / 5, 0], // higher numbers = less movement on mouse move
       0.25, // responseiveness
       delta // (delta) determines how quickly the update occurs. 
     )
@@ -85,4 +87,41 @@ function CameraRig({ children }) {
   )  
 }
 
-useGLTF.preload("/baseball_hat.glb");
+// load the model itself
+function Shirt(props) {
+  const snap = useSnapshot(state);
+  const { nodes, materials } = useGLTF(`/shirt_baked.glb`);
+  // for textures
+  const texture = useTexture(`/${snap.selectedDecal}.png`)
+     // smooths out the color change
+     useFrame((state, delta) =>
+     easing.dampC(materials.lambert1.color, snap.selectedColor, 0.25, delta)
+   )
+  
+  materials.lambert1.color = new THREE.Color,(snap.selectedColor); // uses the color from the state
+
+  return (
+    <mesh
+      castShadow
+      geometry={nodes.T_Shirt_male.geometry}
+      material={materials.lambert1}
+      material-roughness={1}
+      {...props}
+      dispose={null}>
+      <Decal
+        position={[0, 0.04, 0.15]}
+        rotation={[0, 0, 0]}
+        scale={0.15}
+        opacity={0.7}
+        map={texture}
+        map-anisotropy={16}
+      />
+    </mesh>
+  )
+}
+
+          
+
+useGLTF.preload(`/shirt_baked.glb`)
+  ;[`/react.png`, `/three2.png`, `/pmndrs.png`].forEach(useTexture.preload)
+
--- src/Overlay.js
+++ src/Overlay.js
@@ -1,49 +1,61 @@
 import { Logo } from '@pmndrs/branding';
-import { AiOutlineHighlight, AiOutlineShopping, AiFillCamera, AiOutlineArrowLeft } from 'react-icons/ai';
+import { AiOutlineHighlight, AiOutlineShopping, AiFillCamera, AiOutlineArrowLeft, AiOutlineSend } from 'react-icons/ai';
+import { useSnapshot } from 'valtio';
+import state from './store';
 
+// this is the contorl for state from valtio
 export default function Overlay() {
-  return <Customizer />
-  
-    
+  const snap = useSnapshot(state); 
+
+
+
+  return (
+    <div className="container">
+      <header>
+        <Logo width="40" height="40" />
+        <div>
+          <AiOutlineShopping size="3em" />
+        </div>
+      </header>
+      {/* controls which page is displayed */}
+      {snap.intro ? <Intro /> : <Customizer />}
+    </div>
+  )
 }
-// transition from intro to configurator page
+    
+{/* transition from intro to configurator page */}
+    
 function Intro() {
-    return (
-        <div className="container">
-            {/* Header */}
-            <header>
-                <Logo width="40" height="40" />
-                <AiOutlineShopping size="3em" />
-            </header>
-            {/* Hero */}
-            <section key="main">
-        <div className="section--container">
+  return (
+    <section key="main">
+      <div className="section--container">
+        <div>
+          <h1>LET'S DO IT.</h1>
+        </div>
+        <div className="support--content">
           <div>
-            <h1>LET'S DO IT.</h1>
-          </div>
-          <div className="support--content">
-            <div>
-              <p>
-                Create your unique and exclusive shirt with our brand-new 3D
-                customization tool. <strong>Unleash your imagination</strong>{' '}
-                and define your own style.
-                </p>
-                
-                {/* Button */}
-              <button style={{ background: 'black' }}>
-                CUSTOMIZE IT <AiOutlineHighlight size="1.3em" />
-              </button>
-            </div>
+            <p>
+              Create your unique and exclusive shirt with our brand-new 3D
+              customization tool. <strong>Unleash your imagination</strong> and
+              define your own style.
+            </p>
+            <button
+              style={{ background: 'black' }}
+              onClick={() => (state.intro = false)}>
+              CUSTOMIZE IT <AiOutlineHighlight size="1.3em" />
+            </button>
           </div>
         </div>
-      </section>
-    </div>
+      </div>
+    </section>
   )
 }
 
 // Customizer page
-// color to pick from
+// colors to pick from
 function Customizer() {
+  const snap = useSnapshot(state)
+
   const colors = [
     '#ccc',
     '#EFBD4E',
@@ -53,7 +65,11 @@ function Customizer() {
     '#353934',
     'Purple'
   ]
+
+  // list of decals
   const decals = ['react', 'three2', 'pmndrs']
+
+
 // creates a div for each object in the array
   return (
     <section key="custom">
@@ -63,25 +79,42 @@ function Customizer() {
             <div
               key={color}
               className="circle"
-              style={{ background: color }}></div> // style is the color
+              style={{ background: color }} // style is the color
+              onClick={() => (state.selectedColor = color)}></div> 
           ))}
         </div>
+        {/* Decals */}
         <div className="decals">
           <div className="decals--container">
-            {decals.map((decal) => (
-              <div key={decal} className="decal">
+            {snap.decals.map((decal) => (
+              <div
+                key={decal} className="decal"
+                onClick={() => (state.selectedDecal = decal)}>
                 <img src={decal + '_thumb.png'} alt="brand" />
               </div>
             ))}
           </div>
         </div>
-        <button className="share" style={{ background: 'black' }}>
+        {/* Butttons */}
+        
+        <button className="download"
+          style={{ background: snap.selectedColor }}>
           DOWNLOAD
-          <AiFillCamera size="1.3em" />
+          <AiFillCamera size="1.3em" />  {/* Icon from react-icons */}
         </button>
-        <button className="exit" style={{ background: 'black' }}>
+
+        <button
+          className="exit"
+          style={{ background: snap.selectedColor }}
+          onClick={() => (state.intro = true)}
+          >
           GO BACK
-          <AiOutlineArrowLeft size="1.3em" />
+          <AiOutlineArrowLeft size="1.3em" />  {/* Icon from react-icons */}
+        </button>
+
+        <button className="share" style={{ background: snap.selectedColor }}>
+          Share
+          <AiOutlineSend size="1.3em" />  {/* Icon from react-icons */}
         </button>
       </div>
     </section>
